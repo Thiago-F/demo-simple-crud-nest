61f15a8c36634ff8d325930c2b6653f807f99a8a
Thiago-F <thiago_fialho150@hotmail.com>
2023-05-18 10:57:50 -0300

    chore: add mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/crud-app', {}),
+    UsersModule,
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })
