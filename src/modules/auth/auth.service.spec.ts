f40b0ac06f4e058e8c8dfe5cad1106bf6d43959a
Thiago-F <thiago_fialho150@hotmail.com>
2023-05-26 09:24:43 -0300

    test: validateUser

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -3,9 +3,19 @@ import { AuthService } from './auth.service';
 import { UserRepository } from '../../data/repositories/user-repository';
 import { EncryptAdapter } from '../../infra/bcryptAdapter';
 import { JwtService } from '@nestjs/jwt';
+import { UnauthorizedException } from '@nestjs/common';
+import { UserEntity } from '../users/entities/user.entity';
+
+const makeFakeUser = (): UserEntity => ({
+  id: 1,
+  name: 'any_name',
+  email: 'any_email@mail.com'
+} as UserEntity)
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let sut: AuthService;
+  let encryptAdapter: EncryptAdapter;
+  let userRepository: UserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,14 +24,14 @@ describe('AuthService', () => {
         {
           provide: UserRepository,
           useValue: {
-            findOne: jest.fn(),
+            findOne: jest.fn().mockResolvedValue(makeFakeUser()),
             create: jest.fn()
           }
         },
         {
           provide: EncryptAdapter,
           useValue: {
-            compare: jest.fn(),
+            compare: jest.fn().mockResolvedValue(true),
             hash: jest.fn()
           }
         },
@@ -34,10 +44,34 @@ describe('AuthService', () => {
       ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    sut = module.get<AuthService>(AuthService);
+    encryptAdapter = module.get<EncryptAdapter>(EncryptAdapter);
+    userRepository = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(sut).toBeDefined();
+  });
+
+  describe('validateUser', () => {
+    it('should return null if user does not found', async () => {
+      jest.spyOn(userRepository, 'findOne').mockResolvedValueOnce(null)
+      const result = await sut.validateUser('any_email@mail.com', 'any_password')
+      expect(result).toBeNull()
+    });
+
+    it('should throw if password does not match', async () => {
+      jest.spyOn(encryptAdapter, 'compare').mockResolvedValueOnce(null)
+      const promise = sut.validateUser('any_email@mail.com', 'any_password')
+      await expect(promise).rejects.toThrowError(new UnauthorizedException())
+    });
+
+    it('should return a user entity without password on success', async () => {
+      const result = await sut.validateUser('any_email@mail.com', 'any_password')
+      expect(result).toEqual({
+        ...makeFakeUser(),
+        password: undefined
+      })
+    });
   });
 });
