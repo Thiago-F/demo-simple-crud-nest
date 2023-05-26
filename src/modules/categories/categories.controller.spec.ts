db9870ff936b4d5a960befd7faba0a91e3c47d57
Thiago-F <thiago_fialho150@hotmail.com>
2023-05-26 16:18:22 -0300

    test: category controller

--- src/modules/categories/categories.controller.spec.ts
+++ src/modules/categories/categories.controller.spec.ts
@@ -1,9 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { UserEntity } from '../users/entities/user.entity';
+import { CategoryEntity } from './entities/category.entity';
+
+const makeFakeUser = (): UserEntity => ({
+  id: 1,
+  name: 'any_name',
+  email: 'any_email@mail.com'
+} as UserEntity)
+
+const makeFakeCategory = (): CategoryEntity => ({
+  id: 1,
+  name: 'any_name'
+} as CategoryEntity)
 
 describe('CategoriesController', () => {
-  let controller: CategoriesController;
+  let sut: CategoriesController;
+  let categoriesService: CategoriesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,18 +26,93 @@ describe('CategoriesController', () => {
         {
           provide: CategoriesService,
           useValue: {
-            findOne: jest.fn(),
-            findAll: jest.fn(),
-            create: jest.fn(),
+            create: jest.fn().mockResolvedValue(makeFakeCategory()),
+            findAll: jest.fn().mockResolvedValue([makeFakeCategory(), makeFakeCategory()]),
+            findOne: jest.fn().mockResolvedValue(makeFakeCategory()),
           }
         },
       ],
-    }).compile();
+    }).compile(); 
 
-    controller = module.get<CategoriesController>(CategoriesController);
+    sut = module.get<CategoriesController>(CategoriesController);
+    categoriesService = module.get<CategoriesService>(CategoriesService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(sut).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should call service with correct values', async () => {
+      const createSpy = jest.spyOn(categoriesService, 'create')
+
+      await sut.create(
+        {
+          name: 'any_name'
+        },
+        {
+          user: makeFakeUser()
+        }
+      )
+
+      expect(createSpy).toHaveBeenCalledWith({
+        data: {
+          name: 'any_name'
+        },
+        user: makeFakeUser()
+      })
+    });
+
+    it('should return a category entity on success', async () => {
+      const result = await sut.create(
+        {
+          name: 'any_name'
+        },
+        {
+          user: makeFakeUser()
+        }
+      )
+
+      expect(result).toEqual(makeFakeCategory())
+    });
+  });
+
+  describe('findAll', () => {
+    it('should call service with correct values', async () => {
+      const findAllSpy = jest.spyOn(categoriesService, 'findAll')
+
+      await sut.findAll({ name: 'any_name' }, { user: makeFakeUser() })
+
+      expect(findAllSpy).toHaveBeenCalledWith({
+        filters: {
+          name: 'any_name'
+        },
+        user: makeFakeUser()
+      })
+    });
+
+    it('should return a list of category entities on success', async () => {
+      const response = await sut.findAll({ name: 'any_name' }, { user: makeFakeUser() })
+
+      expect(response).toEqual([makeFakeCategory(), makeFakeCategory()])
+    });
+  });
+
+  describe('findOne', () => {
+    it('should call service with correct values', async () => {
+      const findOneSpy = jest.spyOn(categoriesService, 'findOne')
+
+      await sut.findOne('1')
+
+      expect(findOneSpy).toHaveBeenCalledWith({
+        id: Number('1')
+      })
+    });
+
+    it('should return a category entity on success', async () => {
+      const response = await sut.findOne('1')
+
+      expect(response).toEqual(makeFakeCategory())
+    });
   });
 });
--- src/modules/categories/entities/category.entity.ts
+++ src/modules/categories/entities/category.entity.ts
@@ -1,1 +1,12 @@
-export class Category {}
+import { Category } from "@prisma/client";
+
+export class CategoryEntity implements Category {
+    id: number;
+    name: string;
+    createdAt: Date;
+    createdBy: number;
+    updatedAt: Date;
+    updatedBy: number;
+    deletedAt: Date;
+    deletedBy: number;
+}
